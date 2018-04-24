ac1f56e10eafcb5fcff6d7a590f9c908971e62c0
Camron Cohen <Cam@ip-107-230.wireless.oberlin.edu>
2018-04-24 12:58:16 -0400

    Finished drawing board and fixing camera Still need to add animations and draw pieces

--- BoardImg.cpp
+++ BoardImg.cpp
@@ -5,10 +5,12 @@
 
 #include "BoardImg.hpp"
 
-int width = 500;
-int height = 300;
+int width = 900;
+int height = 400;
 float distance = 100;
 
+//called once at beginning
+//initializes environment
 void init(int argc, char ** argv) {
     glutInit(&argc, argv);
 
@@ -29,12 +31,14 @@ void init(int argc, char ** argv) {
 
 }
 
+//called when window is reshaped
 void reshape(int newWidth, int newHeight) {
     width = newWidth;
     height = newHeight;
 
     glViewport(0, 0, width, height);
 
+    //realligns perspective projection transformation
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     float aspect = (float)width / (float)height;
@@ -42,27 +46,58 @@ void reshape(int newWidth, int newHeight) {
 
 }
 
+//called every frame, divided into update and render
 void display(void){
     update();
     render();
 }
 
+//updates any logic before rendering
+    //includes moving pieces, and awarding king values
 void update() {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-    gluLookAt(30.0, 0.0, 0.0, 0, 0, 0, 0.0, 1.0, 0.0);
+    gluLookAt(70.0, 80.0, -80.0, 70.0, 0.0, 50.0, 0.0, 1.2, 1.0);
 }
 
 void render() {
-    glColor4f(1.0, 0.0, 0.0, 1.0);
-    glBegin(GL_QUADS);
-        glVertex3f(0.0, -10.0, -10.0);
-        glVertex3f(0.0, -10.0, 10.0);
-        glVertex3f(0.0, 10.0, 10.0);
-        glVertex3f(0.0, 10.0, -10.0);
-    glEnd();
+
+    //puts translation matrix on so we have offset
+        //makes placing peices easier
+    glPushMatrix();
+    glTranslated(-10.0, 0.0, -10.0);
+
+    //creates board
+    int z; //goes through z coordinates
+    int x; //goes through x coordinates
+    float scale = 20.0;
+    for (z = 0; z < 8; z++) {
+        for (x = 0; x < 8; x++) {
+            //handles color
+            if ((z + x) % 2 == 0)
+                glColor4f(1.0, 0.0, 0.0, 1.0);
+            else
+                glColor4f(0.0, 0.0, 0.0, 1.0);
+
+            glBegin(GL_QUADS);
+                glVertex3f(x * scale, 0.0, z * scale);
+                glVertex3f(x * scale + scale, 0.0, z * scale);
+                glVertex3f(x * scale + scale, 0.0, z * scale + scale);
+                glVertex3f(x * scale, 0.0, z * scale + scale);
+            glEnd();
+        }
+    }
+    glPopMatrix();
+
+
+    /*glBegin(GL_QUADS);
+        glVertex3f(-10.0, 0.0, -10.0);
+        glVertex3f(-10.0, 0.0, 10.0);
+        glVertex3f(10.0, 0.0, 10.0);
+        glVertex3f(10.0, 0.0, -10.0);
+    glEnd();*/
 
     glutSwapBuffers();
 }
--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ FLAGS = -g -Wall -Wextra -g
 
 OGL = -framework OpenGL -framework GLUT -framework Carbon
 
-BoardImg: BoardImg.cpp BoardImg.h
+BoardImg: BoardImg.cpp BoardImg.hpp
 	g++ $(FLAGS) $(OGL) -o BoardImg BoardImg.cpp
