4c420175f3533dd788a66e7202c631d2ffee3c8a
Camron Cohen <Cam@ip-107-230.wireless.oberlin.edu>
2018-04-24 20:42:49 -0400

    Changed to be .hpp

---
+++ CheckersAI/CheckersAI/Global.hpp
@@ -0,0 +1,22 @@
+//
+//  Global.h
+//  CheckersAI
+//
+//  Created by Kenny Wang on 4/12/18.
+//  Copyright © 2018 Kenny Wang. All rights reserved.
+//
+
+#ifndef Global_h
+#define Global_h
+
+const int BOARD_LENGTH = 8;
+
+
+enum Piece {
+    EMPTY,
+    WHITE, WHITE_KING,
+    BLACK, BLACK_KING
+    
+};
+
+#endif /* Global_h */
