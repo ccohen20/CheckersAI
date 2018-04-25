c1c799bbe32efa87fd0fca6b160fa582031b8edd
Camron Cohen <Cam@ip-107-230.wireless.oberlin.edu>
2018-04-24 20:39:12 -0400

    Changed organization to match standard c++ style Fixed this errors caused by not dereferencing this

--- CheckersAI/CheckersAI/Pieces.cpp
+++ CheckersAI/CheckersAI/Pieces.cpp
@@ -7,48 +7,13 @@
 
 #include "Pieces.hpp"
 
-class Piece {
-    char Color;
-    int isKing;
-    
-    int xTile;
-    int yTile;
-    
-public:
-    void makePiece(char Color, int xTile, int yTile);
-    void makeKing();
-   
-    int isKing(){
-        return isKing;
-    }
-    
-    char getColor(){
-        return this.Color;
-    }
-    
-    int getX(){
-        return this.xTile;
-    }
-    
-    int getY(){
-        return this.yTile;
-    }
-}
-
-void Piece::Piece (char Color, int xTile, int yTile){
-    this.Color = Color;
-    this.xTile = xTile;
-    this.yTile = yTile;
-    this.isKing = 0;
+Piece::Piece (char Color, int xTile, int yTile){
+    this->Color = Color;
+    this->xTile = xTile;
+    this->yTile = yTile;
+    this->isKing = 0;
 }
 
 void Piece::makeKing () {
-    this.isKing = 1;
-}
-
-void Piece::makePiece (char Color, int xTile, int yTile){
-    this.Color = Color;
-    this.xTile = xTile;
-    this.yTile = yTile;
-    this.isKing = 0;
+    this->isKing = 1;
 }
--- CheckersAI/CheckersAI/Pieces.hpp
+++ CheckersAI/CheckersAI/Pieces.hpp
@@ -10,4 +10,21 @@
 
 #include <stdio.h>
 
+class Piece {
+    private:
+        char Color;
+        int isKing;
+        
+        int xTile;
+        int yTile;
+    
+    public:
+        Piece(char Color, int xTile, int yTile);
+        void makeKing();
+        int isKing(){return this->isKing;}
+        char getColor(){return this->Color;}
+        int getX(){return this->xTile;}
+        int getY(){return this->yTile;}
+};
+
 #endif /* Pieces_hpp */
