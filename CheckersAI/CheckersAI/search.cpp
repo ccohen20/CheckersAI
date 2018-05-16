c008e4b319416dc432b9a4df46e248b6cf304bb6
Robinson Lamont <Robby@ip-91-132.wireless.oberlin.edu>
2018-05-15 21:12:28 -0400

    implemented minimax

--- CheckersAI/CheckersAI/search.cpp
+++ CheckersAI/CheckersAI/search.cpp
@@ -1,93 +1,115 @@
 #include <iostream>
 #include "Board.hpp"
 
-search(Board board, int depth){
+Move search(Board board, int depth, int bot){
 
-	bestMove= 0;
+    int BIGNEG = -100000;
+    int BIGPOS = 100000;
+    
+    int becomeKing = 5;
+    int kill = 3;
+    int center = 1;
+    
+    vector<Move> moves;
+    moves = getMoves(board,bot);
 
-	for(int i=0; i<BOARD_LENGTH; i++){
-		for(int j=0; j<BOARD_LENGTH; j++) {
-			if(board[i][j]==piece){
-				board.validMove
-			}
-		}
-	}
+    Move bestMove;
+    int bestVal;
 
-	//vector <board> moveList;
-	vector <int> moveList;
-	//implement this, babyyyyyy
-	moveList = getAllMoves(moveList);
+    int size = moves.size();
+
+    if(bot){
+        bestVal = BIGNEG;
+        for(int i = 0; i < size; i++){
+            Move move = moves[i];
+            Board temp = board.copy();
 
 
+            temp.movePiece(move.oldX,move.oldY,move.newX,move.newY);
+            //min
+            int moveVal = minmax(temp, depth-1, 0);
+            //maximizing worst outcomes
+            if(moveVal > bestVal){
+                bestVal = moveVal;
+                bestMove = move;
+            }
+        }
+    } else {
+        //player
+        bestVal = BIGPOS;
+        for(int i = 0; i < size; i++){
+            Move move = moves[i];
+            Board temp = board.copy();
 
-	if(moveList.size()==0) {
-		bestMove = -1;
-		int bestScore=-1000000;
-	
+            temp.movePiece(move.oldX,move.oldY,move.newX,move.newY);
+            //max
+            int moveVal = minmax(temp, depth-1, 1);
+            //minimizing best outcomes
+            if(moveVal < bestVal){
+                bestVal = moveVal;
+                bestMove = move;
+            }
+        }
 
+    }
+    }
 
-	for(int i=0;i<moveList.size();i++) {
-			int move=moveList[i].move;
-			makeMove(moveList[i]);				
-			int next=minn(depth-1);
-			if(next > bestScore) {
-				bestScore=next;
-				bestMove=moveList[i];
-			}
-			unmakeMove();
-		}
-	} else {
+int minmax(Board b, int depth, int bot){
+    //best bot move
+    if(bot){
+        //basecase
+        if(depth==0){
+            return scoreBoard(b,1);
+        } 
+        else {
+            vector<Move> moves = b.getMoves(b,0);
+            int size = moves.size();
+            int bestVal = BIGNEG;
+            for(int i = 0; i < size; i++){
+                Move move = moves[i];
+                Board temp = board.copy();
 
-		vector <MOVE> moveList;
-		genallmoves(moveList);
-				
-		if(moveList.size() == 0)
-		bestMove.move=-1;
-		int bestScore=1000000;
-		for(int i=0;i<moveList.size();i++) {
-			int move=moveList[i].move;
-			makeMove(moveList[i]);
-			int next=maxx(depth-1);
-			if(next < bestScore) {
-				bestScore=next;
-				bestMove=moveList[i];
-			}
-			unmakeMove();
-		}
-	}
-	return bestMove;
+                temp.movePiece(move.oldX,move.oldY,move.newX,move.newY);
+                //min
+                int moveVal = minmax(temp, depth-1, 0);
+                //maximizing worst outcomes
+                if(moveVal > bestVal){
+                    bestVal = moveVal;
+                }
+            }
+            return bestVal;
+        }
+    }
+    //best player move 
+    else {
+        //basecase
+        if(depth==0){
+            return scoreBoard(b,1);
+        } 
+        else {
+            vector<Move> moves = b.getMoves(b,1);
+            int size = moves.size();
+            int bestVal = BIGPOS;
+            for(int i = 0; i < size; i++){
+                Move move = moves[i];
+                Board temp = board.copy();
+
+                temp.movePiece(move.oldX,move.oldY,move.newX,move.newY);
+                //min
+                int moveVal = minmax(temp, depth-1, 1);
+                //minimizing best outcomes
+                if(moveVal < bestVal){
+                    bestVal = moveVal;
+                }
+            }
+            return bestVal;
+        }
+    }
 }
 
-int maxx(int depth) {
+    
 
-	if(depth==0)
-		return evaluate();vector <MOVE> moveList;
-	genallmoves(moveList);
-	int bestScore=-100000;
-	for(int i=0;i<moveList.size();i++)
-	{
-		int move=moveList[i].move;
-		makeMove(moveList[i]);
-		bestScore=max(bestScore,minn(depth-1));
-		unmakeMove();
-	}
-	
-	return bestScore;
-}
-// Min subroutine for MinMax function
-int minn(int depth)
-{
-	if(depth==0)
-		return evaluate();
-	vector <MOVE> moveList;
-	genallmoves(moveList);
-	int bestScore=+100000;
-	for(int i=0;i<moveList.size();i++)
-	{
-		int move=moveList[i].move;
-		makeMove(move);
-		bestScore=min(bestScore,maxx(depth-1));
-		unmakeMove();
-	}
-	return bestScore;
-}
+
+
+
+
